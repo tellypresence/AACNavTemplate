a98a02ee1aafbbfb2f8d5bc1f0804554b3705467
tellypresence <praktique-githubpub01@yahoo.com>
2018-06-24 17:33:00 +0100

    Configure navigation drawer activity for AAC/Jetpack "Navigation" component

--- app/src/main/java/com/example/navtemplate/activity/MainActivity.kt
+++ app/src/main/java/com/example/navtemplate/activity/MainActivity.kt
@@ -6,13 +6,49 @@ import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.navtemplate.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    lateinit var navController: NavController
+    val navigationView by lazy { nav_view }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d(TAG, "onSupportNavigateUp()")
+        // Ref: https://developer.android.com/reference/androidx/navigation/ui/NavigationUI
+        // This _should_ be correct for case w/nav drawer
+        return NavigationUI.navigateUp(drawer_layout, navController)
+    }
+
+    /**
+     * Set up nav drawer menu items to use AAC "Navigation" component
+     *
+     * NOTE: based on T 'n E guesswork, docs woefully inadequate
+     *
+     * @precondition MUST CALL AFTER ToolBar (nee action/app bar) setup
+     *
+     * Refs:
+     *      Ref: https://developer.android.com/reference/androidx/navigation/ui/NavigationUI
+     *
+     * Steve Madsen, 9 Jun 2018
+     */
+    private fun setupNavGraph() {
+        navController = findNavController(R.id.frag_my_nav_graph)
+        // Calls onNavDestinationSelected(MenuItem, NavController) when menu item selected
+        NavigationUI.setupWithNavController(navigationView, navController)
+        // Changes title, animates hamburger to back/up icon
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +59,8 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        setupNavGraph()
     }
 
     override fun onBackPressed() {
