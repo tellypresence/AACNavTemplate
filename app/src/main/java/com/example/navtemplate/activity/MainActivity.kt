4302a218d55a4badcbeca01ff35b4ce69d44b5e9
tellypresence <praktique-githubpub01@yahoo.com>
2018-06-24 17:16:41 +0100

    Replace androidx w/legacy
    AndroidX may be the future but not nearly ready for use yet, replete w/conflicts

--- app/src/main/java/com/example/navtemplate/activity/MainActivity.kt
+++ app/src/main/java/com/example/navtemplate/activity/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.navtemplate.activity
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.core.view.GravityCompat
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.example.navtemplate.R
