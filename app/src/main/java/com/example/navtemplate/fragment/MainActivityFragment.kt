bccb2da5bc18f2c4c73c816c89e98a7c15eeb140
tellypresence <praktique-githubpub01@yahoo.com>
2018-06-24 17:38:56 +0100

    Navigate to "Game Over" via button
    NOTE: button stops working after repeated nav to "Game Over" and back to main frag,
    but the nav drawer "Game Over" item continues to work OK

--- app/src/main/java/com/example/navtemplate/fragment/MainActivityFragment.kt
+++ app/src/main/java/com/example/navtemplate/fragment/MainActivityFragment.kt
@@ -5,15 +5,29 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.navtemplate.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class MainActivityFragment : Fragment() {
 
+    lateinit var rootView: View
+    val buttonDummy by lazy { button_dummy_nav }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        rootView = inflater.inflate(R.layout.fragment_main, container, false)
+        return rootView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        buttonDummy.setOnClickListener {
+            Navigation.findNavController(rootView).navigate(R.id.action_mainActivityFragment_to_gameOver)
+
+        }
     }
 }
